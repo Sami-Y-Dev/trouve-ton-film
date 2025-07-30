f2005db78bca26eabd2c6347751196a917916fac
Sami-Y-Dev <sami.y.dev@gmail.com>
2025-07-30 18:27:44 +0200

    Mise en place du système d'authentification

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,24 +6,26 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Faker\Factory;
 
 class UserFixtures extends Fixture
 {
-    public function __construct(private UserPasswordHasherInterface $hasher) {}
+    public function __construct(private UserPasswordHasherInterface $passwordHasher) {}
 
     public function load(ObjectManager $manager): void
     {
-        $faker = Factory::create();
+        $admin = new User();
+        $admin->setEmail('admin@administrateur.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'adminpass'));
+        $manager->persist($admin);
+        $this->addReference('user_0', $admin);
 
-        for ($i = 0; $i < 5; $i++) {
+        for ($i = 1; $i <= 4; $i++) {
             $user = new User();
-            $user->setEmail($faker->unique()->email());
+            $user->setEmail("user@utilisateur.com");
             $user->setRoles(['ROLE_USER']);
-            $user->setPassword($this->hasher->hashPassword($user, 'password'));
-
+            $user->setPassword($this->passwordHasher->hashPassword($user, 'userpass'));
             $manager->persist($user);
-
             $this->addReference('user_' . $i, $user);
         }
 
